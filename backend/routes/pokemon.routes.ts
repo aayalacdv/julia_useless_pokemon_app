ad0cff7cfbec8153c22f50905e029f316e17ca52
Julia Alos <juliaalos@gmail.com>
2022-03-05 12:51:45 +0100

    pokemon.routes.ts

--- backend/routes/pokemon.routes.ts
+++ backend/routes/pokemon.routes.ts
@@ -1,10 +1,15 @@
-import { Express } from 'express'; 
-
-
+import { Express } from 'express';
+import {
+    getAllHandler,
+    getTwoRandomHandler,
+    updateOneHandler,
+  } from "../src/controller/pokemon.controller";
 
 export default function( app: Express){
 
     //define routes
-
+    app.get('/api/pokemons', getAllHandler);
+    app.get('/api/random', getTwoRandomHandler);
+    app.put('/api/votes', updateOneHandler);
 
 }
