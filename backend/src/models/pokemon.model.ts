daa5afd33266691c1058fe36f90fa5a53a0fa916
Julia Alos <juliaalos@gmail.com>
2022-03-05 12:30:39 +0100

    pokemon model

--- backend/src/models/pokemon.model.ts
+++ backend/src/models/pokemon.model.ts
@@ -2,27 +2,27 @@ import mongoose, { Schema, Model, Document } from "mongoose";
 
 //Primeramente creamos una interfaz con los datos del modelo
 //es aconsejable exportar la interfaz para poder usarla en otros archivos
-export interface IBook extends Document {
+export interface IPokemon extends Document {
   name: string;
-  author: string;
-  price: number;
+  imgUrl: string;
+  votes: number;
 }
 
 
 //Cremaos un schema y lo asignamos al tipo de la interfaz anterior
-const bookSchema = new Schema<IBook>(
+const pokemonSchema = new Schema<IPokemon>(
   {
 //Definimos las propiedades que pusimos antes en la interfaz
     name: { type: String, required: true },
-    author: { type: String, required: true },
-    price: { type: Number, required: true, min: 0 },
+    imgUrl: { type: String, required: true },
+    votes: { type: Number, required: true, min: 0 },
   },
 //podemos pasar otro objeto de manera opcional con otras propiedades que pone la librería
   { timestamps: true }
 );
 
 
-// definimos un modelo que es lo que usaremos para hacer queries a la base de datos y lo que tendrá toddas las propiedades definidas antes. 
-const Book = mongoose.model('Book',bookSchema); 
+// definimos un modelo que es lo que usaremos para hacer queries a la base de datos y lo que tendrá toddas las propiedades definidas antes.
+const Pokemon = mongoose.model('Pokemon',pokemonSchema);
 
-export default Book; 
+export default Pokemon;
