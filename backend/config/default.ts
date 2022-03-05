3bdc4d12abf82ff3d03e3ee0dd293f9a703cb29b
aayalacdv <aayalacdv2000@gamil.com>
2022-03-05 12:14:53 +0100

    added backend folders

---
+++ backend/config/default.ts
@@ -0,0 +1,8 @@
+export default {
+    //host y puerto donde correrá el servidor
+    host: '0.0.0.0',
+    //URI de conexión a la base de datos
+    port: 8080, 
+    dbUri: 'mongodb://mongo:27017/seminario',
+
+}
